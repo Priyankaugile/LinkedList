e6fcf5fc46d9d26c8d7df8e18f6202bb99551eaf
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-02 01:38:02 +0530

    Added: Code of Linked list.

---
+++ linkedlist/Master.java
@@ -0,0 +1,13 @@
+package com.linkedlist;
+
+public class Master {
+
+        public static void main(String[] args) {
+
+            System.out.println("Data Structure\n" +
+                    "Problems using\n" +
+                    "Java Generics");
+        }
+    }
+
+
